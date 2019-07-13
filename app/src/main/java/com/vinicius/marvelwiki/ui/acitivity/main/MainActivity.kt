8640e00227f5431343ed55714f18cea6a1f7feaa
Vinicius Coscia <vinicius.coscia@live.com>
2019-07-13 09:24:30 -0300

    RxJava2 funcionando com Retrofit

--- app/src/main/java/com/vinicius/marvelwiki/ui/acitivity/main/MainActivity.kt
+++ app/src/main/java/com/vinicius/marvelwiki/ui/acitivity/main/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.vinicius.marvelwiki.ui.acitivity.main
 
 import android.os.Bundle
-import br.com.domain.model.Character
-import br.com.domain.model.CharactersRequest
+import android.util.Log
 import br.com.remote.service.IMarvelAPI
 import br.com.utils.Constants
 import com.vinicius.marvelwiki.R
 import com.vinicius.marvelwiki.commons.BaseActivity
 import dagger.android.AndroidInjection
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
 
     @Inject
     lateinit var api: IMarvelAPI
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AndroidInjection.inject(this)
 
-        api.getAllCharacters(IMarvelAPI.getTimeInMillis(), Constants.PUBLIC_KEY, IMarvelAPI.getHash())
-            .subscribe()
+        val a = api.getAllCharacters(IMarvelAPI.getTimeInMillis(), Constants.PUBLIC_KEY, IMarvelAPI.getHash())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+
+        val b =
+            a.subscribe { charRequest -> Log.d("TEXTO", (charRequest.data.results[1].name))}
+
     }
 
 }
+
